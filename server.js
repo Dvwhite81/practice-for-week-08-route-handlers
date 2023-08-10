359cda4e397cf5106badc269ff50451d0dbf8a82
Dan <dvwhite1981@gmail.com>
2023-08-09 22:34:30 -0400

    Complete

--- server.js
+++ server.js
@@ -35,6 +35,70 @@ const server = http.createServer((req, res) => {
 
     // define route handlers here
 
+    // GET /
+    if (req.method === 'GET' && req.url === '/') {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      return res.end('Dog Club');
+    }
+
+    // GET /dogs
+    if (req.method === 'GET' && req.url === '/dogs') {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      return res.end('Dogs index');
+    }
+
+    // Out of order so it checks before dogId
+    // GET /dogs/new
+    if (req.method === 'GET' && req.url === '/dogs/new') {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      return res.end('Dog create form page');
+    }
+
+    let urlSubs = req.url.split('/');
+    let urlLength = urlSubs.length;
+    let targetSubs = '/dogs/:dogId'.split('/');
+    let targetLength = targetSubs.length;
+
+    if (req.method === 'GET' && req.url.startsWith('/dogs')) {
+
+      // GET /dogs/:dogId
+      if (targetLength === urlLength) {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end(`Dog details for ${targetSubs[2]}:`)
+      }
+
+      // GET /dogs/:dogId/edit
+      if (targetLength === urlLength - 1) {
+        console.log(urlSubs);
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end(`Dog edit form page for ${urlSubs[2]}`)
+      }
+    }
+
+    let newId = getNewDogId();
+
+    // POST /dogs
+    if (req.method === 'POST' && req.url === '/dogs') {
+      res.statusCode = 302;
+      res.setHeader('Location', `/dogs/${newId}`);
+      return res.end();
+    }
+
+    // POST /dogs/:dogId
+    if (req.method === 'POST' && req.url.startsWith('/dogs')) {
+
+      if (targetLength === urlLength) {
+        res.statusCode = 302;
+        res.setHeader('Location', `/dogs/${targetSubs[2]}`);
+        return res.end();
+      }
+    }
+
+
     // Do not edit below this line
     // Return a 404 response when there is no matching route handler
     res.statusCode = 404;
